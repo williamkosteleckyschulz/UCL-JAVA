a962b7a85d81f214c466cc7a6327aa5c1f7e8604
William Schulz <79381540+williamkosteleckyschulz@users.noreply.github.com>
2021-05-12 08:50:06 +0100

    Add files via upload

---
+++ Equation.java
@@ -0,0 +1,14 @@
+public class Equation
+{
+    public static void main(String[] args)
+    {
+        //main method begins execution of Java application
+
+        int x = 7;
+        int y = 3;
+        int result = x / y;
+        System.out.printf("The value of x is: %d \n", x); // %d placeholder for an int
+        System.out.printf("The value of y is: %d\n", y);
+        System.out.printf("The value of result is: %d\n", result);
+    }// end method main
+}// end class Greeter
