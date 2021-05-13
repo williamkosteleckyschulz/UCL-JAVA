f3620523033a9cb2fbbcd650123ca07bf9401780
William Schulz <79381540+williamkosteleckyschulz@users.noreply.github.com>
2021-05-13 13:34:48 +0100

    Add files via upload

---
+++ Greeter.java
@@ -0,0 +1,10 @@
+//Text printing program
+
+public class Greeter
+{
+    public static void main(String[] args)
+    {
+        //main method begins execution of Java application
+        System.out.println("Programming will help me succeed!");
+    }// end method main
+}// end class Greeter
